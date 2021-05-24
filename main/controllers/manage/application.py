7c2282b8d0d357c08d0314fe375f6bc2b908cd1e
logan <maxcl730@gmail.com>
2021-05-24 16:56:20 +0800

    debug loan

--- main/controllers/manage/application.py
+++ main/controllers/manage/application.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import sys
-from datetime import datetime, timedelta
-from flask import current_app, Blueprint, request, abort, jsonify, flash, redirect
+from flask import current_app, Blueprint, request, url_for, jsonify, flash, redirect
 from flask_security import login_required
 from common.helper import ops_render
-from main.models.member import Member
+# from main.models.member import Member
 from database import db
 from main.models.application import Application
 from main.models.loan import Repayment
@@ -41,14 +40,15 @@ def list_application(page=1):
     return ops_render('manage/application/index.html', response_data)
 
 
-@application_bp.route("/change_application_status", methods=['POST'])
+@application_bp.route("/change_application_status/<int:application_id>/<int:status>", methods=['GET'])
 @login_required
-def change_application_status():
-    data = request.get_json()
-    application = Application.query.filter_by(id=data.get('id', 0)).first()
+def change_application_status(application_id=0, status=0):
+    Log.info(application_id)
+    Log.info(status)
+    application = Application.query.filter_by(id=application_id).first()
     if application:
-        if data.get('status') == '1':
-            application.status = int(data.get('status'))
+        if status == 1:
+            application.status = int(status)
             ml = MonthInstallment(corpus=application.amount,
                                   periods=application.term,
                                   y_rate=application.apr,
@@ -58,16 +58,24 @@ def change_application_status():
             for installment in ml.installments(start_date=Date.today_date()):
                 new_repayment = Repayment(application_id=application.id,
                                           term=installment['sequence'],
-                                          payment_due_date=installment['date'],
+                                          payment_due_date=installment['payment due date'],
                                           fee=installment['fee'])
                 db.session.add(new_repayment)
             db.session.add(application)
             db.session.commit()
-        elif data.get('status') == '2':
-            application.status = int(data.get('status'))
+            flash('Application has been approved.')
+            return redirect(url_for('manage_application.list_application'))
+        elif status == 2:
+            application.status = int(status)
             db.session.add(application)
             db.session.commit()
+            flash('Application has been rejected.')
+            return redirect(url_for('manage_application.list_application'))
         # Log.info(application.status)
-        return jsonify({'code': 0, 'message': 'success', 'data': {}})
+        #return jsonify({'code': 0, 'message': 'success', 'data': {
+        #    'id': application.id, 'status':  int(status)
+        #}})
     else:
-        return jsonify({'code': -1, 'message': 'failure', 'data': {}})
+        # return jsonify({'code': -1, 'message': 'failure', 'data': {}})
+        flash('Application operating was failed!')
+        return redirect(url_for('manage_application.list_application'))
