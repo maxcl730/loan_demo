ff5e129001e1b2006f07e87de05945d5c364424b
logan <maxcl730@gmail.com>
2021-11-15 14:53:31 +0800

    debug

--- test/yakeen_test2.py
+++ test/yakeen_test2.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 from suds.client import Client
+from suds import WebFault
 #from suds.sudsobject import asdict
-#from suds import WebFault
 #from suds.transport import TransportError
-import sys
 import json
 import logging
-from pprint import pprint
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('suds.client').setLevel(logging.INFO)
@@ -20,25 +18,29 @@ yk_client = Client(sap_url)
 #print(yk_client)
 #sys.exit()
 #print("#"*80)
+user_info = dict()
+user_address_info = dict()
 
-citizenInfoRequest = yk_client.factory.create("citizenInfoRequest")
 nin = '1081383117'
 birthday = '12-1414'
+citizenInfoRequest = yk_client.factory.create("citizenInfoRequest")
 citizenInfoRequest.chargeCode = 'PILOT'
-citizenInfoRequest.dateOfBirth = birthday # '1997-11-01'
+citizenInfoRequest.dateOfBirth = birthday  # '1997-11-01'
 citizenInfoRequest.nin = nin  # '1101193801'
 citizenInfoRequest.password = 'Gccpay@39077'
 citizenInfoRequest.userName = 'Gccpay_PILOT_USR'
 citizenInfoRequest.referenceNumber = 'loan'
-
-rs_CitizenInfo = yk_client.service.getCitizenInfo(citizenInfoRequest)
-user_info = dict()
-for key in rs_CitizenInfo.__keylist__:
-    user_info[key] = rs_CitizenInfo[key]
-print("#"*200)
-#print(user_info)
-print(json.dumps(user_info))
-print("#"*200)
+try:
+    rs_CitizenInfo = yk_client.service.getCitizenInfo(citizenInfoRequest)
+    user_info.clear()
+    for key in rs_CitizenInfo.__keylist__:
+        user_info[key] = rs_CitizenInfo[key]
+    print("#" * 200)
+    # print(user_info)
+    print(json.dumps(user_info))
+    print("#" * 200)
+except WebFault as e:
+    print(e)
 
 citizenAddressInfoRequest = yk_client.factory.create("citizenAddressInfoRequest")
 nin = '1081383117'
@@ -50,25 +52,24 @@ citizenAddressInfoRequest.password = 'Gccpay@39077'
 citizenAddressInfoRequest.userName = 'Gccpay_PILOT_USR'
 citizenAddressInfoRequest.referenceNumber = 'loan'
 citizenAddressInfoRequest.addressLanguage = 'E'
-
-rs_CitizenAddressInfo = yk_client.service.getCitizenAddressInfo(citizenAddressInfoRequest)
-#print(rs_CitizenAddressInfo)
-user_address_info = dict()
-print("#"*200)
-for key in rs_CitizenAddressInfo.__keylist__:
-    user_address_info[key] = rs_CitizenAddressInfo[key]
-    if isinstance(user_address_info[key], list):
-        for v in user_address_info[key]:
-            user_address_info[type(v).__name__] = list()
-            user_address_detail = dict()
-            if hasattr(v, '__keylist__') and hasattr(v, '__iter__'):
-                user_address_detail = {add_key: v[add_key] for add_key in v.__keylist__}
-                #user_address_detail['class'] = type(v).__name__
-                user_address_info[type(v).__name__].append(user_address_detail)
-#print(rs_CitizenAddressInfo)
-print(json.dumps(user_address_info))
-#print(user_address_info)
-print("#"*200)
+try:
+    rs_CitizenAddressInfo = yk_client.service.getCitizenAddressInfo(citizenAddressInfoRequest)
+    user_address_info.clear()
+    print("#"*200)
+    for key in rs_CitizenAddressInfo.__keylist__:
+        if isinstance(rs_CitizenAddressInfo[key], list):
+            user_address_detail = list()
+            for v in rs_CitizenAddressInfo[key]:
+                if hasattr(v, '__keylist__') and hasattr(v, '__iter__'):
+                    user_address_detail.append({add_key: v[add_key] for add_key in v.__keylist__})
+            user_address_info[type(v).__name__] = user_address_detail
+        else:
+            user_address_info[key] = rs_CitizenAddressInfo[key]
+    print(json.dumps(user_address_info))
+    # print(user_address_info)
+    print("#"*200)
+except WebFault as e:
+    print(e)
 
 getAlienInfoByIqamaRequest = yk_client.factory.create("alienInfoByIqamaRequest")
 iqamaNumber = '2475836777'
@@ -79,15 +80,17 @@ getAlienInfoByIqamaRequest.iqamaNumber = iqamaNumber  # '1101193801'
 getAlienInfoByIqamaRequest.password = 'Gccpay@39077'
 getAlienInfoByIqamaRequest.userName = 'Gccpay_PILOT_USR'
 getAlienInfoByIqamaRequest.referenceNumber = 'loan'
-
-rs_AlienInfo = yk_client.service.getAlienInfoByIqama(getAlienInfoByIqamaRequest)
-user_info = dict()
-for key in rs_AlienInfo.__keylist__:
-    user_info[key] = rs_AlienInfo[key]
-print("#"*200)
-#print(user_info)
-print(json.dumps(user_info))
-print("#"*200)
+try:
+    rs_AlienInfo = yk_client.service.getAlienInfoByIqama(getAlienInfoByIqamaRequest)
+    user_info.clear()
+    for key in rs_AlienInfo.__keylist__:
+        user_info[key] = rs_AlienInfo[key]
+    print("#"*200)
+    # print(user_info)
+    print(json.dumps(user_info))
+    print("#"*200)
+except WebFault as e:
+    print(e)
 
 getAlienAddressInfoRequest = yk_client.factory.create("alienAddressInfoRequest")
 iqamaNumber = '2475836777'
@@ -99,20 +102,21 @@ getAlienAddressInfoRequest.password = 'Gccpay@39077'
 getAlienAddressInfoRequest.userName = 'Gccpay_PILOT_USR'
 getAlienAddressInfoRequest.referenceNumber = 'loan'
 getAlienAddressInfoRequest.addressLanguage = 'E'
-
-rs_AlienAddressInfo = yk_client.service.getAlienAddressInfo(getAlienAddressInfoRequest)
-user_address_info = dict()
-print("#"*200)
-for key in rs_AlienAddressInfo.__keylist__:
-    user_address_info[key] = rs_AlienAddressInfo[key]
-    if isinstance(user_address_info[key], list):
-        for v in user_address_info[key]:
-            user_address_info[type(v).__name__] = list()
-            user_address_detail = dict()
-            if hasattr(v, '__keylist__') and hasattr(v, '__iter__'):
-                user_address_detail = {add_key: v[add_key] for add_key in v.__keylist__}
-                #user_address_detail['class'] = type(v).__name__
-                user_address_info[type(v).__name__].append(user_address_detail)
-#print(json.dumps(user_address_info))
-print(json.dumps(user_address_info))
-print("#"*200)
+try:
+    rs_AlienAddressInfo = yk_client.service.getAlienAddressInfo(getAlienAddressInfoRequest)
+    user_address_info.clear()
+    print("#"*200)
+    for key in rs_AlienAddressInfo.__keylist__:
+        if isinstance(rs_AlienAddressInfo[key], list):
+            user_address_detail = list()
+            for v in rs_AlienAddressInfo[key]:
+                if hasattr(v, '__keylist__') and hasattr(v, '__iter__'):
+                    user_address_detail.append({add_key: v[add_key] for add_key in v.__keylist__})
+            user_address_info[type(v).__name__] = user_address_detail
+        else:
+            user_address_info[key] = rs_CitizenAddressInfo[key]
+    print(json.dumps(user_address_info))
+    # print(user_address_info)
+    print("#"*200)
+except WebFault as e:
+    print(e)
